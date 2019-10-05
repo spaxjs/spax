aa0ceca717b2f1a64974b73579f8a7025fe08bf9
crossjs <liwenfu@crossjs.com>
2019-10-05 11:42:06 +0800

    test: fix test

--- packages/plugin-auth/test/index.spec.tsx
+++ packages/plugin-auth/test/index.spec.tsx
@@ -6,9 +6,11 @@ import PluginAuth from "../src";
 const blocks = [
   {
     authority: ["foo"],
+    component: () => null,
     blocks: [
       {
         authority: ["bar"],
+        component: () => null,
       },
     ],
   },
@@ -17,7 +19,10 @@ const blocks = [
 test("auth", async () => {
   const rendered = await run(
     [PluginAuth],
-    { blocks, plugins: { auth: { Forbidden: () => "403" } } },
+    {
+      blocks,
+      plugins: { auth: { useAuth: () => false, Forbidden: () => "403" } },
+    },
   );
   expect(rendered[0].authority).toEqual(["foo"]);
   expect(typeof rendered[0].component).toBe("function");
